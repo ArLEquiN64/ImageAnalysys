2f31c6ebe8bcc93fecefc81dee86a8b54871bef0
ArLE <arlequin.prx@gmail.com>
2015-03-12 00:35:03 +0900

    initialize GUI

--- ImageAnalysysGUI/Form1.Designer.cs
+++ ImageAnalysysGUI/Form1.Designer.cs
@@ -23,12 +23,60 @@
 		/// コード エディターで変更しないでください。
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.previewImage = new System.Windows.Forms.PictureBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			((System.ComponentModel.ISupportInitialize)(this.previewImage)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// previewImage
+			// 
+			this.previewImage.Location = new System.Drawing.Point(12, 27);
+			this.previewImage.Name = "previewImage";
+			this.previewImage.Size = new System.Drawing.Size(256, 256);
+			this.previewImage.TabIndex = 0;
+			this.previewImage.TabStop = false;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileFToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(496, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileFToolStripMenuItem
+			// 
+			this.fileFToolStripMenuItem.Name = "fileFToolStripMenuItem";
+			this.fileFToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+			this.fileFToolStripMenuItem.Text = "File(F)";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(496, 409);
+			this.Controls.Add(this.previewImage);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.previewImage)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox previewImage;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileFToolStripMenuItem;
 	}
 }
 
