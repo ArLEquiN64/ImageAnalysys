5e6579b27a03e57bfdc9451f0e18d410d608e9bc
ArLE <arlequin.prx@gmail.com>
2015-03-16 19:18:56 +0900

    refactor complete IFS

--- ImageAnalyser/ImageController.cs
+++ ImageAnalyser/ImageController.cs
@@ -12,6 +12,19 @@ namespace ImageAnalyser {
 		private readonly Bitmap _originalImg;
 		private readonly Color[,] _originalPixelData;
 		private double[,] _grayScaleData;
+		public double[,] GrayScaleData {
+			get { return _grayScaleData; }
+		}
+		private readonly int _width;
+
+		public int width {
+			get { return _width; }
+		}
+		private readonly int _height;
+		public int height {
+			get { return _height; }
+		}
+
 
 		public ImageController(string filePath = DefaultPath) {
 			Console.Write("opening file...");
@@ -20,11 +33,11 @@ namespace ImageAnalyser {
 			_outputPath = _filePath + DateTime.Now.Ticks + @"\";
 			Directory.CreateDirectory(_outputPath);
 			_originalImg = new Bitmap(_filePath + _filename);
-			int w = _originalImg.Width;
-			int h = _originalImg.Height;
-			_originalPixelData = new Color[w, h];
-			for (int y = 0; y < h; y++) {
-				for (int x = 0; x < w; x++) {
+			_width = _originalImg.Width;
+			_height = _originalImg.Height;
+			_originalPixelData = new Color[width, height];
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
 					_originalPixelData[x, y] = _originalImg.GetPixel(x, y);
 				}
 			}
@@ -34,46 +47,52 @@ namespace ImageAnalyser {
 
 		public void SaveToFile(string fileName, Color[,] newPixelData) {
 			Console.Write("saveing...");
-			if (File.Exists(_outputPath + fileName)) {
-				Console.WriteLine("\nError : "+fileName+"is already exists.");
-				return;
+			var outputPath = _outputPath + _filename.Insert(_filename.LastIndexOf('.'), "_" + fileName);
+			if (File.Exists(outputPath)) {
+				/*
+				Console.WriteLine("\nError : " + fileName + "is already exists.\n");
+				return
+				 */
+				int i = 0;
+				while (File.Exists(outputPath.Insert(outputPath.LastIndexOf('.'), outputPath+" - " + i))) {
+					i++;
+				}
+				outputPath = outputPath.Insert(outputPath.LastIndexOf('.'), outputPath + " - " + i);
 			}
-			int width = newPixelData.GetLength(0);
-			int height = newPixelData.GetLength(1);
-			using (var saveImg = new Bitmap(width, height)) {
-				for (int y = 0; y < height; y++) {
-					for (int x = 0; x < width; x++) {
+			int w = newPixelData.GetLength(0);
+			int h = newPixelData.GetLength(1);
+			using (var saveImg = new Bitmap(w, h)) {
+				for (int y = 0; y < h; y++) {
+					for (int x = 0; x < w; x++) {
 						saveImg.SetPixel(x, y, newPixelData[x, y]);
 					}
 				}
-
-				var outputPath = _outputPath + DateTime.Now.Ticks + "_" + _filename.Insert(_filename.LastIndexOf('.'), "_" + fileName);
 				saveImg.Save(outputPath, ImageFormat.Bmp);
-				Console.WriteLine("succsess : " + _filename.Insert(_filename.LastIndexOf('.'), "_" + fileName));
+				Console.WriteLine("succsess : " + outputPath.Substring(outputPath.LastIndexOf('\\') + 1));
 			}
 		}
 
 		public void SaveToFile(string fileName, double[,] newPixelData) {
 			Console.Write("exchange to Color...");
-			int width = newPixelData.GetLength(0);
-			int height = newPixelData.GetLength(1);
-			var saveImg = new Color[width, height];
-			for (int y = 0; y < height; y++) {
-				for (int x = 0; x < width; x++) {
+			int w = newPixelData.GetLength(0);
+			int h = newPixelData.GetLength(1);
+			var saveImg = new Color[w, h];
+			for (int y = 0; y < h; y++) {
+				for (int x = 0; x < w; x++) {
 					saveImg[x, y] = Color.FromArgb(255, (int) newPixelData[x, y], (int) newPixelData[x, y], (int) newPixelData[x, y]);
 				}
 			}
 			SaveToFile(fileName, saveImg);
 		}
 
-		public void MakeGrayScale() {
+		private void MakeGrayScale() {
 			Console.Write("Makeing Gray Scale...");
 			int width = _originalPixelData.GetLength(0);
 			int height = _originalPixelData.GetLength(1);
 			var _grayScaleData = new double[width, height];
 			for (var y = 0; y < height; y++) {
 				for (var x = 0; x < width; x++) {
-					_grayScaleData[x, y] = (int)(_originalPixelData[x, y].GetBrightness() * 255);
+					_grayScaleData[x, y] = (int) (_originalPixelData[x, y].GetBrightness() * 255);
 				}
 			}
 			Console.WriteLine("succsess");
